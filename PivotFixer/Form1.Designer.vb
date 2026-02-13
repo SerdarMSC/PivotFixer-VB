9efac086e92c827df3d56aabc33063799b3e3142
MSC <MSC@DESKTOP-TAC641O>
2026-02-13 14:43:37 +0300

    Add project files.

---
+++ PivotFixer/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnSelect = New Button()
+        btnStart = New Button()
+        lblStatus = New Label()
+        progressBar1 = New ProgressBar()
+        lblResult = New Label()
+        SuspendLayout()
+        ' 
+        ' btnSelect
+        ' 
+        btnSelect.Location = New Point(12, 12)
+        btnSelect.Name = "btnSelect"
+        btnSelect.Size = New Size(198, 39)
+        btnSelect.TabIndex = 0
+        btnSelect.Text = "Dosya Sec"
+        btnSelect.UseVisualStyleBackColor = True
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(216, 12)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(71, 39)
+        btnStart.TabIndex = 1
+        btnStart.Text = "Basla"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' lblStatus
+        ' 
+        lblStatus.AutoSize = True
+        lblStatus.Location = New Point(12, 67)
+        lblStatus.Name = "lblStatus"
+        lblStatus.Size = New Size(43, 15)
+        lblStatus.TabIndex = 2
+        lblStatus.Text = "Hazir..."
+        ' 
+        ' progressBar1
+        ' 
+        progressBar1.Location = New Point(12, 107)
+        progressBar1.Name = "progressBar1"
+        progressBar1.Size = New Size(275, 23)
+        progressBar1.TabIndex = 3
+        ' 
+        ' lblResult
+        ' 
+        lblResult.AutoSize = True
+        lblResult.Location = New Point(14, 89)
+        lblResult.Name = "lblResult"
+        lblResult.Size = New Size(41, 15)
+        lblResult.TabIndex = 4
+        lblResult.Text = "Label1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(298, 145)
+        Controls.Add(lblResult)
+        Controls.Add(progressBar1)
+        Controls.Add(lblStatus)
+        Controls.Add(btnStart)
+        Controls.Add(btnSelect)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MdiChildrenMinimizedAnchorBottom = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "Pivot Data Duzenleyici"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnSelect As Button
+    Friend WithEvents btnStart As Button
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents progressBar1 As ProgressBar
+    Friend WithEvents lblResult As Label
+
+End Class
---
+++ PivotFixer/Form1.vb
@@ -0,0 +1,206 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Xml.Linq
+Imports System.Threading.Tasks
+
+Public Class Form1
+
+    Private _files As New List(Of String)
+    Private _totalPivotCaches As Integer = 0
+    Private _totalPivotTables As Integer = 0
+
+    ' -------------------------------------------------
+    ' DOSYA / KLASÖR SEÇ
+    ' -------------------------------------------------
+    Private Sub btnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+
+        _files.Clear()
+
+        Using dlg As New OpenFileDialog()
+            dlg.Filter = "Excel Files (*.xlsx)|*.xlsx"
+            dlg.Multiselect = True
+
+            If dlg.ShowDialog() = DialogResult.OK Then
+                _files.AddRange(dlg.FileNames)
+            End If
+        End Using
+
+        If _files.Count = 0 Then
+            Using fbd As New FolderBrowserDialog()
+                If fbd.ShowDialog() = DialogResult.OK Then
+                    _files.AddRange(Directory.GetFiles(fbd.SelectedPath, "*.xlsx"))
+                End If
+            End Using
+        End If
+
+        lblStatus.Text = _files.Count & " dosya seçildi"
+        lblResult.Text = ""
+
+    End Sub
+
+    ' -------------------------------------------------
+    ' BAŞLAT
+    ' -------------------------------------------------
+    Private Async Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        If _files.Count = 0 Then
+            MessageBox.Show("Önce dosya veya klasör seçin.")
+            Return
+        End If
+
+        btnStart.Enabled = False
+        progressBar1.Value = 0
+        progressBar1.Maximum = _files.Count
+        progressBar1.Step = 1
+
+        _totalPivotCaches = 0
+        _totalPivotTables = 0
+
+        Await Task.Run(Sub()
+
+                           For Each file In _files
+                               Try
+                                   ProcessXlsx(file)
+                               Catch
+                                   ' Hatalı dosya atlanır
+                               End Try
+
+                               Invoke(Sub()
+                                          progressBar1.PerformStep()
+                                          lblStatus.Text = System.IO.Path.GetFileName(file)
+                                      End Sub)
+                           Next
+
+                       End Sub)
+
+        lblStatus.Text = "Tamamlandı"
+        lblResult.Text = "Pivot Cache: " & _totalPivotCaches &
+                         "   |   Pivot Table: " & _totalPivotTables
+
+        btnStart.Enabled = True
+        MessageBox.Show("Tüm dosyalar işlendi.")
+
+    End Sub
+
+    ' -------------------------------------------------
+    ' XLSX İŞLE (YEDEK + FIX)
+    ' -------------------------------------------------
+    Private Sub ProcessXlsx(xlsxPath As String)
+
+        ' ----- YEDEK KLASÖRÜ -----
+        Dim backupFolder As String =
+            System.IO.Path.Combine(
+                System.IO.Path.GetDirectoryName(xlsxPath),
+                "backup"
+            )
+
+        If Not Directory.Exists(backupFolder) Then
+            Directory.CreateDirectory(backupFolder)
+        End If
+
+        Dim backupName As String =
+            System.IO.Path.GetFileNameWithoutExtension(xlsxPath) &
+            "_" & DateTime.Now.ToString("yyyyMMdd_HHmmss") &
+            ".xlsx"
+
+        Dim backupPath As String =
+            System.IO.Path.Combine(backupFolder, backupName)
+
+        File.Copy(xlsxPath, backupPath, True)
+
+        ' ----- TEMP KLASÖR -----
+        Dim tempDir As String =
+            System.IO.Path.Combine(
+                System.IO.Path.GetTempPath(),
+                "pivotfix_" & Guid.NewGuid().ToString()
+            )
+
+        Directory.CreateDirectory(tempDir)
+
+        ' ----- ZIP AÇ -----
+        ZipFile.ExtractToDirectory(xlsxPath, tempDir)
+
+        _totalPivotCaches += FixPivotCaches(tempDir)
+        _totalPivotTables += FixPivotTables(tempDir)
+
+        ' ----- ZIP GERİ OLUŞTUR -----
+        File.Delete(xlsxPath)
+
+        ZipFile.CreateFromDirectory(
+            tempDir,
+            xlsxPath,
+            CompressionLevel.Optimal,
+            False
+        )
+
+        Directory.Delete(tempDir, True)
+
+    End Sub
+
+    ' -------------------------------------------------
+    ' SAVE SOURCE DATA = OFF
+    ' -------------------------------------------------
+    Private Function FixPivotCaches(root As String) As Integer
+
+        Dim count As Integer = 0
+
+        Dim folderPath As String =
+            System.IO.Path.Combine(root, "xl", "pivotCache")
+
+        If Not Directory.Exists(folderPath) Then Return 0
+
+        For Each file In Directory.GetFiles(folderPath, "pivotCacheDefinition*.xml")
+
+            Dim doc As XDocument = XDocument.Load(file)
+
+            If doc.Root IsNot Nothing Then
+                doc.Root.SetAttributeValue("saveData", "0")
+                count += 1
+            End If
+
+            doc.Save(file)
+
+        Next
+
+        Return count
+
+    End Function
+
+    ' -------------------------------------------------
+    ' REFRESH ON OPEN = ON
+    ' -------------------------------------------------
+    Private Function FixPivotTables(root As String) As Integer
+
+        Dim count As Integer = 0
+
+        Dim folderPath As String =
+            System.IO.Path.Combine(root, "xl", "pivotTables")
+
+        If Not Directory.Exists(folderPath) Then Return 0
+
+        For Each file In Directory.GetFiles(folderPath, "pivotTableDefinition*.xml")
+
+            Dim doc As XDocument = XDocument.Load(file)
+
+            If doc.Root IsNot Nothing Then
+
+                Dim attr = doc.Root.Attribute("refreshOnLoad")
+
+                If attr IsNot Nothing Then
+                    attr.Value = "1"
+                Else
+                    doc.Root.Add(New XAttribute("refreshOnLoad", "1"))
+                End If
+
+                count += 1
+            End If
+
+            doc.Save(file)
+
+        Next
+
+        Return count
+
+    End Function
+
+End Class
